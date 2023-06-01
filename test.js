c0f78d6a5c20cbef0bd97a74970db054c44fe1a1
takashi <pero857942632@gmail.com>
2023-06-02 00:31:06 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,10 @@
+/*
+ * プロジェクトフォルダ直下で
+ * 以下のいずれかのコマンドで実行可能
+ * node test
+ * node test.js
+ * node ./test.js
+*/
+let x = "test"
+let y = "テスト"
+console.log(x + y);
---
+++ test2.js
@@ -0,0 +1,10 @@
+/*
+ * プロジェクトフォルダ直下で
+ * 以下のいずれかのコマンドで実行可能
+ * node test2
+ * node test2.js
+ * node ./test2.js
+*/
+let x = "test2"
+let y = "テスト2"
+console.log(x + y);
